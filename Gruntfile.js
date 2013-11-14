360938dca86f54d0082e09d6fb36c2f13ddac042
dmccarthy <dmccarthy@tssg.org>
2013-11-14 12:43:17 +0000

    first commit

---
+++ Gruntfile.js
@@ -0,0 +1,98 @@
+'use strict';
+
+module.exports = function (grunt) {
+
+   var tests       = 'test/**/*_test.js';
+   var lib         = 'lib/**/*.js';
+
+   // Project configuration.
+   grunt.initConfig({
+      pkg: grunt.file.readJSON('package.json'),
+      clean: {
+         build:['build']
+      },
+      nodeunit: {
+         files: ['test/**/*_test.js']
+      },
+      watch : {
+         files : [ lib, tests ],
+         tasks : 'default'
+      },
+      plato: {
+         options: {
+            title: 'OPENi Logger',
+            jshint: grunt.file.readJSON('.jshintrc')
+         },
+         metrics: {
+            files: {
+               'build/metrics': [ lib ]
+            }
+         }
+      },
+      instrument : {
+         files : [lib],
+         options : {
+            lazy : true,
+            excludes : tests,
+            basePath : 'build/instrument/',
+            flatten : false
+         }
+      },
+      reloadTasks : {
+         rootPath : 'build/instrument/lib'
+      },
+      storeCoverage : {
+         options : {
+            dir : 'build/reports/'
+         }
+      },
+      makeReport : {
+         src : 'build/reports/**/*.json',
+         options : {
+            reporters : {
+               'lcov'     :{dir:'build/reports/'},
+               'cobertura':{dir:'build/reports/'}
+            },
+            print : 'detail'
+
+         }
+      },
+      coverage: {
+         options: {
+            thresholds: {
+               'statements': 90,
+               'branches'  : 90,
+               'lines'     : 90,
+               'functions' : 90
+            },
+            dir: 'reports',
+            root: 'build/'
+         }
+      },
+      jshint: {
+         all: [lib],
+         options: grunt.file.readJSON('.jshintrc')
+      },
+   });
+
+   // These plugins provide necessary tasks.
+   grunt.loadNpmTasks('grunt-contrib-clean');
+   grunt.loadNpmTasks('grunt-contrib-jshint');
+   grunt.loadNpmTasks('grunt-contrib-uglify');
+   grunt.loadNpmTasks('grunt-contrib-nodeunit');
+   grunt.loadNpmTasks('grunt-testem');
+   grunt.loadNpmTasks('grunt-qunit-cov');
+   grunt.loadNpmTasks('grunt-plato');
+   grunt.loadNpmTasks('grunt-node-tap');
+   grunt.loadNpmTasks('grunt-istanbul');
+   grunt.loadNpmTasks('grunt-istanbul-coverage');
+   grunt.loadNpmTasks('grunt-contrib-nodeunit');
+   grunt.loadNpmTasks('grunt-contrib-watch');
+   grunt.loadNpmTasks('grunt-config');
+
+
+   grunt.registerTask('test',     ['nodeunit']);
+   grunt.registerTask('cover',    ['clean:build', 'instrument', 'nodeunit', 'storeCoverage', 'makeReport']);
+   grunt.registerTask('default',  ['jshint',     'nodeunit' ]);
+   grunt.registerTask('jenkins',  ['jshint',      'cover',      'coverage',    'plato']);
+};
---
+++ README.md
@@ -0,0 +1,27 @@
+# openi-logger
+
+Logger for the OPENi Cloudlet Platform
+
+## Getting Started
+Install the module with: `npm install openi-logger`
+
+```javascript
+var openi_logger = require('openi-logger');
+openi_logger.awesome(); // "awesome"
+```
+
+## Documentation
+_(Coming soon)_
+
+## Examples
+_(Coming soon)_
+
+## Contributing
+In lieu of a formal styleguide, take care to maintain the existing coding style. Add unit tests for any new or changed functionality. Lint and test your code using [Grunt](http://gruntjs.com/).
+
+## Release History
+_(Nothing yet)_
+
+## License
+Copyright (c) 2013 dmccarthy  
+Licensed under the MIT license.
